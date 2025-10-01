4f1067bbe29f758322a14fb0af7d49503307284f
Fatma Robbaniyah <rfatma096@gmail.com>
2025-10-01 22:24:25 +0700

    first commit

--- app/src/main/java/com/example/pam/MainActivity.kt
+++ app/src/main/java/com/example/pam/MainActivity.kt
@@ -1,42 +1,50 @@
 package com.example.pam
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
         val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
+        val buttonBrowser = findViewById<Button>(R.id.button_browser)
 
+        // Tombol Count
         buttonCountUp.setOnClickListener {
             mCount++
             mShowCount.text = mCount.toString()
         }
 
+        // Tombol Toast
         buttonToast.setOnClickListener {
             val tulisan = mShowCount.text.toString()
-            val toast = Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG)
-            toast.show()
+            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Explicit Intent -> pindah ke MainActivity2
+        buttonSwitchPage.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        // Implicit Intent -> buka browser
+        buttonBrowser.setOnClickListener {
+            val intentbrowse = Intent(Intent.ACTION_VIEW)
+            intentbrowse.data = Uri.parse("https://www.google.com/")
+            startActivity(intentbrowse)
         }
     }
 }
